c8b1ca89c1b659d125a082ee5f3f80f225ed2d02
Murat Can Yaman <147244210+MuratYaman1944@users.noreply.github.com>
2024-07-11 12:02:36 +0300

    O3_6 tamamlandı.

--- R/daily_count_stations_with_parameter_threshold.R
+++ R/daily_count_stations_with_parameter_threshold.R
@@ -2,16 +2,27 @@
 #'
 #' @param parameter_name The name of the parameter.
 #' @param threshold The threshold percentage for data availability (default is 90).
+#' @param season The season for which the data availability is calculated (default is "all").
 #' @export
-daily_count_stations_with_parameter_threshold <- function(parameter_name, threshold = 90) {
+
+
+daily_count_stations_with_parameter_threshold <- function(parameter_name, threshold = 90, season = "all") {
 
   mydb <- dbConnect(RSQLite::SQLite(), "temiz-hava.sqlite")
 
-  query <- paste0("SELECT Istasyon FROM (SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / 365) AS data_percentage FROM daily_detail GROUP BY Istasyon) WHERE data_percentage >= ", threshold)
+  summer_months <- c(04, 05, 06, 07, 08, 09)
+
+  query <- paste0("SELECT Istasyon FROM (SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / ", ifelse(season == "summer", "184", "365"), ") AS data_percentage FROM daily_detail")
+
+  if (season == "summer") {
+    query <- paste0(query, " WHERE strftime('%m', Tarih) IN ('04', '05', '06', '07', '08', '09')")
+  }
+
+  query <- paste0(query, " GROUP BY Istasyon) WHERE data_percentage >= ", threshold)
 
   query_result <- dbGetQuery(mydb, query)
 
   dbDisconnect(mydb)
 
-  return(data.frame(nrow(query_result)))
+  return(data.frame(station_count = nrow(query_result)))
 }
--- extdata/daily_detail/O3_daily_detail.R
+++ extdata/daily_detail/O3_daily_detail.R
@@ -23,7 +23,8 @@ output <- list(O3_1 = list(),
                O3_2 = list(),
                O3_3 = list(),
                O3_4 = list(),
-               O3_5 = list()
+               O3_5 = list(),
+               O3_6 = list()
                )
 
 output$O3_1$result_message <- print(paste(parameter_name,": Veri alınan istasyon listesi" ))
@@ -41,7 +42,7 @@ output$O3_4$data <- daily_count_stations_with_parameter_threshold(parameter_name
 output$O3_5$result_message <- print(paste(parameter_name,": Yaz boyunca %90 veri alınan istasyon listesi" ))
 output$O3_5$data <- daily_list_stations_with_parameter_threshold(parameter_name, threshold = 90,season = "summer")
 
-#output$O3_6$result_message <- print(paste(parameter_name," : Yaz boyunca %90 Veri alınan istasyon sayısı" ))
-#output$O3_6$data <- daily_count_stations_with_parameter_threshold(parameter_name, threshold = 90)
+output$O3_6$result_message <- print(paste(parameter_name," : Yaz boyunca %90 Veri alınan istasyon sayısı" ))
+output$O3_6$data <- daily_count_stations_with_parameter_threshold(parameter_name, threshold = 90,season = "summer")
 
 write_output_to_excel(output, result_o3_daily_excel_file)
