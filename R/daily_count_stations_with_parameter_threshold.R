b625abdebd829a328395eb18cafa535c183efcc4
Murat Can Yaman <147244210+MuratYaman1944@users.noreply.github.com>
2024-07-12 11:25:03 +0300

    O3_8 tamamlandı.

--- R/daily_count_stations_with_parameter_threshold.R
+++ R/daily_count_stations_with_parameter_threshold.R
@@ -4,25 +4,21 @@
 #' @param threshold The threshold percentage for data availability (default is 90).
 #' @param season The season for which the data availability is calculated (default is "all").
 #' @export
-
-
 daily_count_stations_with_parameter_threshold <- function(parameter_name, threshold = 90, season = "all") {
 
   mydb <- dbConnect(RSQLite::SQLite(), "temiz-hava.sqlite")
 
-  summer_months <- c(04, 05, 06, 07, 08, 09)
-
-  query <- paste0("SELECT Istasyon FROM (SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / ", ifelse(season == "summer", "184", "365"), ") AS data_percentage FROM daily_detail")
-
   if (season == "summer") {
-    query <- paste0(query, " WHERE strftime('%m', Tarih) IN ('04', '05', '06', '07', '08', '09')")
+    query <- paste0("SELECT Istasyon FROM (SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL AND strftime('%m', Tarih) IN ('04', '05', '06', '07', '08', '09') THEN 1 ELSE 0 END) * 100 / 183) AS data_percentage FROM daily_detail GROUP BY Istasyon) WHERE data_percentage >= ", threshold)
+  } else if (season == "winter") {
+    query <- paste0("SELECT Istasyon FROM (SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL AND strftime('%m', Tarih) IN ('01', '02', '03', '10', '11', '12') THEN 1 ELSE 0 END) * 100 / 182) AS data_percentage FROM daily_detail GROUP BY Istasyon) WHERE data_percentage >= ", threshold)
+  } else {
+    query <- paste0("SELECT Istasyon FROM (SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / 365) AS data_percentage FROM daily_detail GROUP BY Istasyon) WHERE data_percentage >= ", threshold)
   }
-
-  query <- paste0(query, " GROUP BY Istasyon) WHERE data_percentage >= ", threshold)
 
   query_result <- dbGetQuery(mydb, query)
 
   dbDisconnect(mydb)
 
-  return(data.frame(station_count = nrow(query_result)))
+  return(data.frame(nrow(query_result)))
 }
