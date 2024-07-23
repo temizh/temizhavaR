8fece66b59bd5f1f50dc65f7d54461cc29a80840
Murat Can Yaman <147244210+MuratYaman1944@users.noreply.github.com>
2024-07-23 12:25:01 +0300

    O3_5 completed.

--- R/daily_list_stations_with_parameter_threshold.R
+++ R/daily_list_stations_with_parameter_threshold.R
@@ -1,33 +1,49 @@
-#' List Stations for specified parameter from daily_detail
+#' List Stations for Specified Parameter from daily_detail
+#'
+#' This function lists the stations that have data availability above a specified threshold for a given parameter.
+#' You can filter the results for a specific season or for the entire year.
 #'
 #' @param parameter_name The name of the parameter.
 #' @param threshold The threshold percentage for data availability (default is 90).
+#' @param season The specific season to consider (e.g., "summer" for April to September). If NULL, considers the entire year.
+#' @return A data frame with stations that meet the threshold criteria, sorted by data availability percentage in descending order.
 #' @export
+#'
+#' @examples
+#' daily_list_stations_with_parameter_threshold("PM10", 90, "summer")
+#' daily_list_stations_with_parameter_threshold("SO2", 85)
 
-daily_list_stations_with_parameter_threshold <- function(parameter_name, threshold = 90, season) {
-
-  init.temizhavaR()
-
-  if (0) {
+daily_list_stations_with_parameter_threshold <- function(parameter_name, threshold = 90, season = NULL) {
+  if (!is.null(season)) {
     mydb <- dbConnect(RSQLite::SQLite(), "temiz-hava.sqlite")
 
-    query <- paste0("SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / 365) AS veri_mevcudiyet_yuzdesi
-                   FROM daily_detail
-                   GROUP BY Istasyon
-                   HAVING veri_mevcudiyet_yuzdesi >= ", threshold)
+    if (season == "summer") {
+      # Yaz ayları için filtreleme yapar
+      season_filter <- "AND strftime('%m', Tarih) IN ('04', '05', '06', '07', '08', '09')"
+      days_in_season <- 183  # Yaz sezonundaki gün sayısı (Nisan - Eylül)
+    } else {
+      # Diğer sezonlar için uygun filtreleme yapılabilir
+      stop("Geçersiz sezon parametresi.")
+    }
+
+    query <- paste0("SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / ", days_in_season, ") AS veri_mevcudiyet_yuzdesi
+                    FROM daily_detail
+                    WHERE 1=1 ", season_filter, "
+                    GROUP BY Istasyon
+                    HAVING veri_mevcudiyet_yuzdesi >= ", threshold, "
+                    ORDER BY veri_mevcudiyet_yuzdesi DESC")
 
     query_result <- dbGetQuery(mydb, query)
     dbDisconnect(mydb)
-  }
-  else {
+  } else {
     parameter_name <- gsub('\\"', "", parameter_name)
     query_result <- all_daily_detail_load_from_database(parameter_name) %>%
       select(Istasyon, Tarih, rlang::sym(parameter_name)) %>%
       group_by(Istasyon) %>%
       summarise(veri_mevcudiyet_yuzdesi = round(length(which(!is.na(.data[[parameter_name]]))) / 365 * 100)) %>%
+      filter(veri_mevcudiyet_yuzdesi >= threshold) %>%
       arrange(desc(veri_mevcudiyet_yuzdesi), Istasyon)
   }
-
 
   return(query_result)
 }
--- extdata/daily_detail/O3_daily_detail.R
+++ extdata/daily_detail/O3_daily_detail.R
@@ -20,7 +20,9 @@ if (0) {
 output <- list(O3_1 = list(),
                O3_2 = list(),
                O3_3 = list(),
-               O3_4 = list())
+               O3_4 = list(),
+               O3_5 = list()
+               )
 
 output$O3_1$result_message <- print(paste(parameter_name,": Veri alınan istasyon listesi" ))
 output$O3_1$data <- daily_list_stations_with_parameter(parameter_name)
@@ -34,9 +36,10 @@ output$O3_3$data <- daily_list_stations_with_parameter_threshold(parameter_name,
 output$O3_4$result_message <- print(paste(parameter_name," : %90 Veri alınan istasyon sayısı" ))
 output$O3_4$data <- daily_count_stations_with_parameter_threshold(parameter_name, threshold = 90)
 
-# output$O3_5$result_message <- print(paste(parameter_name,": Yaz boyunca %90 veri alınan istasyon listesi" ))
-# output$O3_5$data <- daily_list_stations_with_parameter_threshold(parameter_name, threshold = 90)
-#
+output$O3_5$result_message <- print(paste(parameter_name,": Yaz boyunca %90 veri alınan istasyon listesi" ))
+output$O3_5$data <- daily_list_stations_with_parameter_threshold(parameter_name, threshold = 90,season = "summer")
+
+
 # output$O3_6$result_message <- print(paste(parameter_name," : Yaz boyunca %90 Veri alınan istasyon sayısı" ))
 # output$O3_6$data <- daily_count_stations_with_parameter_threshold(parameter_name, threshold = 90)
 
