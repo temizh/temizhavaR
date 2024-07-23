2dc903cdae3558a3587b2c357086a89cc8bfb88c
Murat Can Yaman <147244210+MuratYaman1944@users.noreply.github.com>
2024-07-23 12:35:23 +0300

    O3_6 completed.

--- R/daily_count_stations_with_parameter_threshold.R
+++ R/daily_count_stations_with_parameter_threshold.R
@@ -2,12 +2,25 @@
 #'
 #' @param parameter_name The name of the parameter.
 #' @param threshold The threshold percentage for data availability (default is 90).
+#' @param season The specific season to consider (e.g., "summer" for nisan, mayıs, haziran, temmuz, ağustos, eylül).
 #' @export
-daily_count_stations_with_parameter_threshold <- function(parameter_name, threshold = 90) {
+
+daily_count_stations_with_parameter_threshold <- function(parameter_name, threshold = 90, season = NULL) {
 
   mydb <- dbConnect(RSQLite::SQLite(), "temiz-hava.sqlite")
 
-  query <- paste0("SELECT Istasyon FROM (SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / 365) AS data_percentage FROM daily_detail GROUP BY Istasyon) WHERE data_percentage >= ", threshold)
+
+  if (!is.null(season) && season == "summer") {
+
+    season_filter <- "AND strftime('%m', Tarih) IN ('04', '05', '06', '07', '08', '09')"
+    days_in_season <- 183
+  } else {
+
+    season_filter <- ""
+    days_in_season <- 365
+  }
+
+  query <- paste0("SELECT Istasyon FROM (SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / ", days_in_season, ") AS data_percentage FROM daily_detail WHERE 1=1 ", season_filter, " GROUP BY Istasyon) WHERE data_percentage >= ", threshold)
 
   query_result <- dbGetQuery(mydb, query)
 
