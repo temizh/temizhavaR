02e015b02d0a4bfdda05265ec0735a9954655823
MuratYaman1944 <tcmuratyaman1923@gmail.com>
2024-07-26 00:41:03 +0300

    O3_5 completed.

--- R/daily_list_stations_with_parameter_threshold.R
+++ R/daily_list_stations_with_parameter_threshold.R
@@ -1,33 +1,35 @@
-#' List Stations for specified parameter from daily_detail
+#' List Stations for Specified Parameter and Season from daily_detail
 #'
 #' @param parameter_name The name of the parameter.
 #' @param threshold The threshold percentage for data availability (default is 90).
+#' @param season The season for which data availability is calculated (default is NULL). Use "summer" or "winter".
+#' @return A data frame with stations and their data availability percentage.
 #' @export
 
-daily_list_stations_with_parameter_threshold <- function(parameter_name, threshold = 90, season) {
+daily_list_stations_with_parameter_threshold <- function(parameter_name, threshold = 90, season = NULL) {
 
-  init.temizhavaR()
+  parameter_name <- gsub('\\"', "", parameter_name)
+  data <- all_daily_detail_load_from_database(parameter_name)
+  data$Tarih <- as.Date(data$Tarih)
 
-  if (0) {
-    mydb <- dbConnect(RSQLite::SQLite(), "temiz-hava.sqlite")
+  if (!is.null(season) && season == "summer") {
 
-    query <- paste0("SELECT Istasyon, (SUM(CASE WHEN ", parameter_name, " IS NOT NULL THEN 1 ELSE 0 END) * 100 / 365) AS veri_mevcudiyet_yuzdesi
-                   FROM daily_detail
-                   GROUP BY Istasyon
-                   HAVING veri_mevcudiyet_yuzdesi >= ", threshold)
-
-    query_result <- dbGetQuery(mydb, query)
-    dbDisconnect(mydb)
+    summer_months <- c(4, 5, 6, 7, 8, 9)
+    data <- data %>% filter(month(Tarih) %in% summer_months)
+    days_in_season <- length(unique(data$Tarih))
   }
+
   else {
-    parameter_name <- gsub('\\"', "", parameter_name)
-    query_result <- all_daily_detail_load_from_database(parameter_name) %>%
-      select(Istasyon, Tarih, rlang::sym(parameter_name)) %>%
-      group_by(Istasyon) %>%
-      summarise(veri_mevcudiyet_yuzdesi = round(length(which(!is.na(.data[[parameter_name]]))) / 365 * 100)) %>%
-      arrange(desc(veri_mevcudiyet_yuzdesi), Istasyon)
+    days_in_season <- length(unique(data$Tarih))
   }
 
+  query_result <- data %>%
+    select(Istasyon, Tarih, parameter_value = .data[[parameter_name]]) %>%
+    group_by(Istasyon) %>%
+    summarise(veri_mevcudiyet_yuzdesi = round(length(which(!is.na(parameter_value))) / days_in_season * 100)) %>%
+    arrange(desc(veri_mevcudiyet_yuzdesi), Istasyon)
 
-  return(query_result)
+  return(as.data.frame(query_result))
+
 }
+
