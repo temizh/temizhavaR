66d594021bd8e21b4612c53011b8198e8fe96652
MuratYaman1944 <tcmuratyaman1923@gmail.com>
2024-07-27 14:22:00 +0300

    O3_10 completed.

--- extdata/hourly_detail/O3_hourly_detail.R
+++ extdata/hourly_detail/O3_hourly_detail.R
@@ -48,4 +48,7 @@ output$O3_9$result_message <- print(paste(parameter_name," : AOT 40 icin 1 saatl
 output$O3_9$data <- hourly_list_aot40_threshold(start_dates = c("2023-05-01", "2023-04-01"), end_dates = c("2023-07-31", "2023-09-30"),threshold = 90)
 
 
+output$O3_10$result_message <-  print(paste(parameter_name," : AOT 40 icin 1 saatlik degerlerin %90 ve üstü veri alınan istasyon sayısı"))
+output$O3_10$data <- count_stations_aot40_threshold(start_dates = c("2023-05-01", "2023-04-01"), end_dates = c("2023-07-31", "2023-09-30"),threshold = 90)
+
 write_output_to_excel(output, result_o3_hourly_excel_file)
