f7394b75498a3fe1557760317e9ba580fec51558
MuratYaman1944 <tcmuratyaman1923@gmail.com>
2024-08-05 11:05:24 +0300

    Updated CO_4

--- extdata/hourly_detail/CO_hourly_detail.R
+++ extdata/hourly_detail/CO_hourly_detail.R
@@ -19,7 +19,9 @@ if (0) {
 
 output <- list(CO_1 = list(),
                CO_2 = list(),
-               CO_3 = list())
+               CO_3 = list(),
+               CO_4 = list()
+               )
 
 
 output$CO_1$result_message <- print(paste(parameter_name,": Veri alınan istasyon listesi" ))
@@ -30,6 +32,9 @@ output$CO_2$data <- hourly_list_stations_with_parameter_threshold(parameter_name
 
 output$CO_3$result_message <- print(paste(parameter_name," : %90 Veri alınan istasyon sayısı" ))
 output$CO_3$data <- hourly_count_stations_with_parameter_threshold(parameter_name, threshold = 90)
+
+output$CO_4$result_message <- print(paste(parameter_name,"Maksimum gunluk 8 saatlik ortalaması 10 mg/m3'u asan istasyonlar"))
+output$CO_4$data <- calculate_co_exceeding_stations()
 
 
 write_output_to_excel(output, result_co_hourly_excel_file)
