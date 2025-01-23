5f5dd45149cdfac1cdfbc66abd91c1694fc3f1e3
badicev <basakdilaracevik@hotmail.com>
2025-01-23 12:17:14 +0300

    fixed version and port issues

--- extdata/Selenium.R
+++ extdata/Selenium.R
@@ -2,27 +2,37 @@ library(RSelenium)
 library(netstat)
 library(wdman)
 library(uuid)
+library(DBI)
+# sudo docker run -d -p 4445:4444 selenium/standalone-firefox:2.53.1
+
 
 #Yeni bir chromedriver versiyonu oluştuğunda C:\Users\Hp\AppData\Local\binman\binman_chromedriver\win32 buradan lisans belgesini sil
 
-selenium()
-selenium_object <- selenium(retcommand = T, check= F)
+tryCatch(
+  {
+    #https://www.zenrows.com/blog/rselenium#installation
+    selenium_server <- rsDriver(browser = "chrome", port = 4445L, chromever = "latest", verbose = T)
+   
 
-#google chrome
-#binman::list_versions("chromedriver")
+    driver <- selenium_server$client
 
-remote_driver <- rsDriver(browser = "chrome", chromever = "125.0.6422.78", verbose = F, port = free_port())
+    driver$open()
+    print(driver)
+    print("Remote driver opened successfully")
+    url <- "https://sim.csb.gov.tr/STN/STN_Report/StationDataDownloadNew"
+    driver$navigate(url)
 
-#close server
-#remote_driver$server$stop()
+  },
+  error = function(e) {
+    message("Error occurred: ", e)
+    if (exists("driver")) {
+      driver$close()
+    }
+  }
+)
 
-remDr <- remote_driver$client
-
-remDr$open()
-remDr$navigate("https://sim.csb.gov.tr/STN/STN_Report/StationDataDownloadNew")
-
-mydb <- dbConnect(RSQLite::SQLite(), "temiz-hava.sqlite")
-
+DBDIR <- "/home/byte/Desktop/Work/temizhavaR/"
+mydb <- dbConnect(RSQLite::SQLite(), paste0(DBDIR, "temiz-hava.sqlite"))
 
 plaka_list <- list(
   "Adana" = "01",
@@ -109,47 +119,54 @@ plaka_list <- list(
 )
 
 
-# Bölge isimlerini alma
-dropdown12_element <- remDr$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
+wait <- driver$setTimeout(type = "script", milliseconds = 5000) 
+
+dropdown12_element <- tryCatch({
+  driver$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
+}, error = function(e) {
+  message("Element not found within the timeout.")
+  NULL
+})
+print(dropdown12_element)
 dropdown12_element$clickElement()
 Sys.sleep(3)
 
-dropdown12_list_items <- remDr$findElements(using = "css", value = ".k-reset li")
+dropdown12_list_items <- driver$findElements(using = "css", value = ".k-reset li")
 
 bolge_list <- sapply(dropdown12_list_items, function(item) item$getElementText()[[1]])
 bolge_list_dolu <- bolge_list[bolge_list != "" & bolge_list != "None"]
 
 ###CLICK ANOTHER ELEMENT
-element <- remDr$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
+element <- driver$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
 element$clickElement()
 
 # Her bölge için şehir ve istasyon isimlerini al
 for (bolge in bolge_list_dolu) {
 
-  clean_dropdown_element <- remDr$findElement(using = 'xpath', value = '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
+  clean_dropdown_element <- driver$findElement(using = 'xpath', value = '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
   clean_dropdown_element$clickElement()
 
-  clean_dropdown_element <- remDr$findElement(using = 'xpath', value = ' //*[@id="dropdown12-contentDataDowloadNew"]/div[1]/div/div/div/span[2]')
+  clean_dropdown_element <- driver$findElement(using = 'xpath', value = ' //*[@id="dropdown12-contentDataDowloadNew"]/div[1]/div/div/div/span[2]')
   clean_dropdown_element$clickElement()
 
-  dropdown12_element <- remDr$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
+  dropdown12_element <- driver$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
   dropdown12_element$clickElement()
   Sys.sleep(3)
-  bolge_item <- remDr$findElement(using = 'xpath', value = paste0("//li[contains(text(), '", bolge, "')]"))
+  bolge_item <- driver$findElement(using = 'xpath', value = paste0("//li[contains(text(), '", bolge, "')]"))
   bolge_item$clickElement()
   Sys.sleep(3)
 
-  element <- remDr$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
+  element <- driver$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
   element$clickElement()
 
-  clean_dropdown_element <- remDr$findElement(using = 'xpath', value = '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
+  clean_dropdown_element <- driver$findElement(using = 'xpath', value = '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
   clean_dropdown_element$clickElement()
 
-  dropdown1_element <- remDr$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
+  dropdown1_element <- driver$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
   dropdown1_element$clickElement()
   Sys.sleep(3)
 
-  dropdown1_list_items <- remDr$findElements(using = "css", value = ".k-reset li")
+  dropdown1_list_items <- driver$findElements(using = "css", value = ".k-reset li")
   sehir_list <- sapply(dropdown1_list_items, function(item) item$getElementText()[[1]])
   sehir_list_dolu <- sehir_list[sehir_list != ""]
 
@@ -157,26 +174,26 @@ for (bolge in bolge_list_dolu) {
 
   for (sehir in sehir_list_dolu) {
 
-    clean_dropdown_element <- remDr$findElement(using = 'xpath', value = '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
+    clean_dropdown_element <- driver$findElement(using = 'xpath', value = '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
     clean_dropdown_element$clickElement()
     Sys.sleep(3)
-    dropdown1_element <- remDr$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
+    dropdown1_element <- driver$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
     dropdown1_element$clickElement()
     Sys.sleep(3)
-    sehir_item <- remDr$findElement(using = 'xpath', value = paste0("//li[contains(text(), '", sehir, "')]"))
+    sehir_item <- driver$findElement(using = 'xpath', value = paste0("//li[contains(text(), '", sehir, "')]"))
     sehir_item$clickElement()
     Sys.sleep(3)
 
-    element <- remDr$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
+    element <- driver$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
     element$clickElement()
 
 
 
-    dropdown_element2 <- remDr$findElement(using = 'xpath', value = '//*[@id="dropdown2-contentDataDowloadNew"]')
+    dropdown_element2 <- driver$findElement(using = 'xpath', value = '//*[@id="dropdown2-contentDataDowloadNew"]')
     dropdown_element2$clickElement()
     Sys.sleep(3)
 
-    dropdown_element_list2 <- remDr$findElements(using = 'css', value = '.k-reset li')
+    dropdown_element_list2 <- driver$findElements(using = 'css', value = '.k-reset li')
     station_list <- sapply(dropdown_element_list2, function(item) item$getElementText()[[1]])
     station_list_dolu <- station_list[station_list != ""]
 
@@ -198,21 +215,20 @@ for (bolge in bolge_list_dolu) {
     }
 
     # Bir sonraki şehir için sayfayı sıfırlamak gerekirse bu noktada işlem yapabilirsiniz
-    element <- remDr$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
+    element <- driver$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
     element$clickElement()
     Sys.sleep(3)
   }
 
   # Bir sonraki bölge için sayfayı sıfırlamak gerekirse bu noktada işlem yapabilirsiniz
-  element <- remDr$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
+  element <- driver$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
   element$clickElement()
   Sys.sleep(3)
 }
 
 
-remDr$close()
-rD$server$stop()
-
+driver$close()
+driver$server$stop()
 
 
 
@@ -320,34 +336,34 @@ rD$server$stop()
 
 
 # Şehir isimlerini alma
-dropdown1_element <- remDr$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
+dropdown1_element <- driver$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
 dropdown1_element$clickElement()
 Sys.sleep(2)
 
-dropdown1_list_items <- remDr$findElements(using = "css", value = ".k-reset li")
+dropdown1_list_items <- driver$findElements(using = "css", value = ".k-reset li")
 sehir_list <- sapply(dropdown1_list_items, function(item) item$getElementText()[[1]])
 sehir_list_dolu <- sehir_list[sehir_list != ""]
 
-element <- remDr$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
+element <- driver$findElement(using = 'xpath', value = '//*[@id="page-wrapper"]/div[1]')
 element$clickElement()
 
 
 # İstasyon isimlerini alma
-dropdown_element2 <- remDr$findElement(using = 'xpath', value = '//*[@id="dropdown2-contentDataDowloadNew"]')
+dropdown_element2 <- driver$findElement(using = 'xpath', value = '//*[@id="dropdown2-contentDataDowloadNew"]')
 dropdown_element2$clickElement()
 Sys.sleep(2)
-dropdown_element_list2 <- remDr$findElements(using = 'css', value = '.k-reset li')
+dropdown_element_list2 <- driver$findElements(using = 'css', value = '.k-reset li')
 station_list <- sapply(dropdown_element_list2, function(item) item$getElementText()[[1]])
 station_list_dolu <- station_list[station_list != ""]
 
 
 
 # Bölge isimlerini alma
-dropdown12_element <- remDr$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
+dropdown12_element <- driver$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
 dropdown12_element$clickElement()
 Sys.sleep(2)
 
-dropdown12_list_items <- remDr$findElements(using = "css", value = ".k-reset li")
+dropdown12_list_items <- driver$findElements(using = "css", value = ".k-reset li")
 
 bolge_list <- sapply(dropdown12_list_items, function(item) item$getElementText()[[1]])
 bolge_list_dolu <- bolge_list[bolge_list != ""]
@@ -377,8 +393,9 @@ dbDisconnect(mydb)
 
 
 
-remDr$close()
-rD$server$stop()
+driver$close()
+driver$server$stop()
+
 
 
 
@@ -395,11 +412,11 @@ rD$server$stop()
 
 #BOLGE ISIMLERI
 
-# dropdown12_element <- remDr$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
+# dropdown12_element <- driver$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
 # dropdown12_element$clickElement()
 # Sys.sleep(2)
 #
-# dropdown12_list_items <- remDr$findElements(using = "css", value = ".k-reset li")
+# dropdown12_list_items <- driver$findElements(using = "css", value = ".k-reset li")
 #
 # bolge_list <- sapply(dropdown12_list_items, function(item) item$getElementText()[[1]])
 # bolge_list_dolu <- bolge_list[bolge_list != ""]
@@ -408,21 +425,21 @@ rD$server$stop()
 
 #SEHIR ISIMLERI
 
-# dropdown1_element <- remDr$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
+# dropdown1_element <- driver$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
 # dropdown1_element$clickElement()
 #  Sys.sleep(2)
 #
-# dropdown1_list_items <- remDr$findElements(using = "css", value = ".k-reset li")
+# dropdown1_list_items <- driver$findElements(using = "css", value = ".k-reset li")
 # sehir_list <- sapply(dropdown1_list_items , function(item) item$getElementText()[[1]])
 # sehir_list_dolu <- sehir_list[sehir_list != ""]
 
 #ISTASYON ISIMLERI
 
-# dropdown_element2 <- remDr$findElement(using = 'xpath', value = '//*[@id="dropdown2-contentDataDowloadNew"]')
+# dropdown_element2 <- driver$findElement(using = 'xpath', value = '//*[@id="dropdown2-contentDataDowloadNew"]')
 # dropdown_element2$clickElement()
 # Sys.sleep(2)
 #
-# dropdown_element_list2 <- remDr$findElements(using = 'css', value = '.k-reset li')
+# dropdown_element_list2 <- driver$findElements(using = 'css', value = '.k-reset li')
 # station_list <- sapply(dropdown_element_list2 , function(item) item$getElementText()[[1]])
 # station_list_dolu <- station_list[station_list != ""]
 
