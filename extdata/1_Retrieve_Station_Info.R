92473684707502fde77ddc662843bd661bdcb9e2
badicev <basakdilaracevik@hotmail.com>
2025-01-23 22:47:11 +0300

    handle missing cities

--- extdata/1_Retrieve_Station_Info.R
+++ extdata/1_Retrieve_Station_Info.R
@@ -41,10 +41,12 @@ tryCatch(
 DBDIR <- "/home/byte/Desktop/Work/temizhavaR/"
 mydb <- dbConnect(RSQLite::SQLite(), paste0(DBDIR, "temiz-hava.sqlite"))
 dbExecute(mydb, "DROP TABLE IF EXISTS location_2023")
-dbExecute(mydb, "CREATE TABLE location_2023 (Bolge TEXT, Sehir TEXT, Plaka TEXT, Istasyonlar TEXT, Id TEXT)")
+
+  
+
 source("extdata/plaka_list.R")
 
-wait <- driver$setTimeout(type = "script", milliseconds = 5000)
+wait <- driver$setTimeout(type = "script", milliseconds = 7000)
 
 dropdown12_element <- tryCatch({
   driver$findElement(using = 'id', value = 'dropdown12-contentDataDowloadNew')
@@ -63,6 +65,10 @@ bolge_list_dolu <- bolge_list[bolge_list != "" & bolge_list != "None"]
 print("Bolge list retrieved")
 
 clickByXpath(driver, '//*[@id="page-wrapper"]/div[1]')
+
+all_cities <- names(plaka_list)
+
+processed_cities <- c()
 
 for (bolge in bolge_list_dolu) {
   print(paste("Processing bolge:", bolge))
@@ -91,6 +97,7 @@ for (bolge in bolge_list_dolu) {
   
   for (sehir in sehir_list_dolu) {
     print(paste("Processing sehir:", sehir))
+    processed_cities <- c(processed_cities, sehir)
     clickByXpath(driver, '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
     Sys.sleep(3)
     
@@ -116,6 +123,10 @@ for (bolge in bolge_list_dolu) {
       id <- UUIDgenerate()
       plaka <- plaka_list[[sehir]]
       
+      
+
+      dbExecute(mydb, "CREATE TABLE IF NOT EXISTS location_2023 (Bolge TEXT, Sehir TEXT, Plaka TEXT, Istasyonlar TEXT, Id TEXT)")
+
       dbExecute(mydb, "INSERT INTO location_2023 (Bolge, Sehir, Plaka, Istasyonlar, Id) VALUES (?, ?, ?, ?, ?)",
                 params = list(bolge, sehir, plaka, istasyon, id))
     }
@@ -128,6 +139,52 @@ for (bolge in bolge_list_dolu) {
   Sys.sleep(3)
 }
 
+missing_cities <- setdiff(all_cities, processed_cities)
+
+if (length(missing_cities) > 0) {
+  print(paste("Processing", length(missing_cities), "cities without region information"))
+  
+  clickByXpath(driver, '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
+  Sys.sleep(3)
+  
+  for (sehir in missing_cities) {
+    print(paste("Processing missing sehir:", sehir))
+    
+    clickByXpath(driver, '//*[@id="dropdown1-contentDataDowloadNew"]/div/div/div/div/span[2]')
+    Sys.sleep(3)
+    
+    dropdown1_element <- driver$findElement(using = 'id', value = 'dropdown1-contentDataDowloadNew')
+    dropdown1_element$clickElement()
+    Sys.sleep(3)
+    
+    sehir_item <- driver$findElement(using = 'xpath', value = paste0("//li[contains(text(), '", sehir, "')]"))
+    sehir_item$clickElement()
+    Sys.sleep(3)
+    
+    clickByXpath(driver, '//*[@id="page-wrapper"]/div[1]')
+    clickByXpath(driver, '//*[@id="dropdown2-contentDataDowloadNew"]')
+    Sys.sleep(3)
+    
+    dropdown_element_list2 <- driver$findElements(using = 'css', value = '.k-reset li')
+    station_list <- sapply(dropdown_element_list2, function(item) item$getElementText()[[1]])
+    station_list_dolu <- station_list[station_list != ""]
+    station_list_dolu <- station_list_dolu[-c(1, 2)]
+    
+    for (istasyon in station_list_dolu) {
+      print(paste("Processing istasyon:", istasyon))
+      id <- UUIDgenerate()
+      plaka <- plaka_list[[sehir]]
+      
+      dbExecute(mydb, "CREATE TABLE IF NOT EXISTS location_2023 (Bolge TEXT, Sehir TEXT, Plaka TEXT, Istasyonlar TEXT, Id TEXT)")
+      
+      dbExecute(mydb, "INSERT INTO location_2023 (Bolge, Sehir, Plaka, Istasyonlar, Id) VALUES (?, ?, ?, ?, ?)",
+                params = list(NA, sehir, plaka, istasyon, id))
+    }
+    
+    clickByXpath(driver, '//*[@id="page-wrapper"]/div[1]')
+    Sys.sleep(3)
+  }
+}
 
 driver$close()
 
