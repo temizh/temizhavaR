9d3b68af14796e4f82bb480a8eae4f9c26ed51f0
badicev <basakdilaracevik@hotmail.com>
2025-03-03 17:30:09 +0300

    update env

--- extdata/DB/read_from_db.R
+++ extdata/DB/read_from_db.R
@@ -3,10 +3,18 @@ library(dplyr)
 library(DBI)
 library(dotenv)
 
-setwd("/home/temizhava/temizhavaR/extdata/DB")
+# setwd("/home/temizhava/temizhavaR/extdata/DB")
 
-# Load environment variables from the .env file
-dotenv::load_dot_env()
+base_dir <- getOption("temizhavaR.base_dir")
+
+env_file <- file.path(base_dir, ".env")
+if (file.exists(env_file)) {
+  dotenv::load_dot_env(file = env_file)
+  cat(".env file loaded successfully.\n")
+} else {
+  stop(".env file not found at: ", env_file)
+}
+
 
 # Read the environment variables
 db_host <- "dev.pranageo.com"
@@ -62,7 +70,7 @@ summary %>% dim()
 summary %>% print(n = 500)
 
 # You can see the actual SQL query 
-summary %>% show_query()
+# summary %>% show_query()
 
 # Tarih value are not clear
 hrly %>% select(Tarih)
