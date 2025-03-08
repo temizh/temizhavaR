5564583c2a46a22684bfef95d40bdefdcd116c9f
badicev <basakdilaracevik@hotmail.com>
2025-03-08 17:17:50 +0300

    dont remove previous data

--- extdata/6_Excel_Files_Merge_Create_DB.R
+++ extdata/6_Excel_Files_Merge_Create_DB.R
@@ -503,7 +503,7 @@ read_and_write_data <- function(delete_previous = FALSE, pattern, overwrite_data
 
 # rows_written <- read_and_write_data()
 # rows_written <- read_and_write_data(pattern = "Adana-Seyhan_saatlik_detay_2014-2024.xlsx")
-rows_written <- read_and_write_data(pattern = "\\.xlsx$", delete_previous = TRUE)
+rows_written <- read_and_write_data(pattern = "\\.xlsx$", delete_previous = FALSE)
 
 
 
